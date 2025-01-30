d9defe4cd7f1cf960636a64eb9b4dccd080a79cd
nehagarg702 <41899788+nehagarg702@users.noreply.github.com>
2025-01-30 21:04:14 +0530

    Delete TrueCallerCopy directory

--- TrueCallerCopy/app/src/main/java/com/example/truecallercopy/dependencyInjection/ViewModelModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.truecallercopy.dependencyInjection
-
-import com.example.truecallercopy.ui.viewmodel.CallerViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val viewModelModule = module {
-    viewModel { CallerViewModel(get()) }
-}
